d13312241363c576673a176f0f9edf4a5c4d39a2
Aaron Snowberger <jekkilekki@gmail.com>
2015-09-21 13:58:23 +0900

    Write odd number range loop

--- PlayingWithLoops.java
+++ PlayingWithLoops.java
@@ -45,6 +45,32 @@ public class PlayingWithLoops {
     }
     
     // #3 Assignment
+    /* 
+     * 1. Ask the user for 2 ints
+     * 2. Display all odd numbers between the ints
+     * (Assume 2nd num is greater)
+     * (HINT: Round even ints to odd, then use for loop)
+     */
+    System.out.println( "\nI will print all the odd numbers between a range you enter.");
+    System.out.println( "Enter the first number: " );
+    int num1 = s.nextInt();
+    System.out.println( "Enter the second number: " );
+    int num2 = s.nextInt();
+    
+    if( num2 < num1 ) {
+      int tmp = num2;
+      num2 = num1; 
+      num1 = tmp;
+    }
+    num1++; // make sure the range is BETWEEN and doesn't include the numbers entered
+    if( num1%2 == 0 ) {
+      num1++;
+    }
+    System.out.print( "Range: [ " );
+    for ( int i = num1; i < num2; i+=2 ) {
+      System.out.print( i + ", " );
+    }
+    System.out.println( "]" );
     
   } // end main method
   
